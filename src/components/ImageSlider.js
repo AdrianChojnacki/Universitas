0f081734c3843cb07e1b99518e975feeb87cc1cd
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-05-12 20:42:06 +0200

    small slider fix

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -24,7 +24,7 @@ const ImageSlider = ({ slides }) => {
       <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
       {SliderData.map((slide, index) => (
         <div className={index === current ? "slide active" : "slide"} key={index}>
-          {index === current && <img src={slide.image} alt={`image${index}`} className="image" />}
+          {index === current && <img src={slide.image} alt={index} className="image" />}
         </div>
       ))}
     </section>
